f21c86c0b8d924660364e97dfc2a73ddf897faf0
Rob Noake <rnoake@pobox.com>
2011-12-09 02:15:32 +0000

    Fix references to arbor components

--- arbor.js
+++ arbor.js
@@ -1,29 +1,36 @@
 var express = require('express'),
     path = require('path'),
-    process = require('process'),
     Settings = require('settings'),
     Winston = require('winston');
 
-module.exports = function (options) {
-    var arbor,
+module.exports = function () {
+    var arbor = {},
         bundleManager,
         logger,
-        server,
-        settings;
+        server;
     
     arbor.settings = new Settings(path.join(process.cwd(), 'config', 'environment.js')).getEnvironment('test');
     
+    console.log('Loaded settings');
+    
     arbor.logger = new Winston.Logger({
-        transports: [new winston.transports.Console({ level: 'silly', timestamp: true, colorize: true })]
+        transports: [new Winston.transports.Console(arbor.settings.logger.console)]
     });
     
-    arbor.bundle = require('./bundle/manager.js');
+    arbor.logger.verbose('Logger initialized');
+    
+    arbor.logger.verbose('Loading bundle manager');    
+    arbor.bundle = require('./bundle/manager.js')(arbor);
+    
+    arbor.settings.bundle.directories.forEach(function (bundleRoot) {
+        arbor.bundle.loadPath(path.join(process.cwd(), bundleRoot));
+    });
     
     arbor.express = express.createServer(
         express.bodyParser(),
         express.methodOverride(),
         express.cookieParser(),
-        express.session({ secret: settings.session.secret })
+        express.session({ secret: arbor.settings.session.secret })
     );
     
     /**
@@ -31,10 +38,10 @@ module.exports = function (options) {
      */
     arbor.start = function () {
         arbor.bundle.finalize(function () {
-            logger.info('Starting HTTP server on port ' + settings.http.port);
-            server.listen(settings.http.port);
+            arbor.logger.info('Starting HTTP server on port ' + arbor.settings.http.port);
+            arbor.express.listen(arbor.settings.http.port);
         });        
     };
     
     return arbor;
-};
+}();
